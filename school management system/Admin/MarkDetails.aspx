8f7aa2828ec91dbb3c8ad361be75378ab8f6bb07
rajmanahmed <rajmanahmed125@gmail.com>
2024-01-01 22:26:11 +0500

    Marks Detials

---
+++ school management system/Admin/MarkDetails.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MarkDetails.aspx.cs" Inherits="school_management_system.Admin.MarkDetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ school management system/Admin/MarkDetails.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class MarkDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["admin"] == null)
+            {
+
+                Response.Redirect("../Login.aspx");
+            }
+        }
+
+    }
+}
